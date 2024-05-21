5bff516e1be783051c4bc2e12397c8d0457193e0
BdNWoG <billy.gao@hotmail.com>
2024-05-20 17:45:39 -0700

    server network

--- harmonic/components/file-upload.tsx
+++ harmonic/components/file-upload.tsx
@@ -2,6 +2,7 @@
 
 import { X } from "lucide-react";
 import { UploadDropzone } from "@/lib/uploadthing";
+import Image from "next/image";
 
 interface FileUploadProps {
     onChange: (url?: String) => void;
@@ -12,6 +13,20 @@ interface FileUploadProps {
 export const FileUpload = ({
     onChange, value, endpoint
 }: FileUploadProps) => {
+    const fileType = value?.split(".").pop();
+
+    if (value && fileType != "pdf") {
+        return (
+            <div className="relative h-20 w-20">
+                <Image src={value} alt="Upload" className="rounded-full" fill /> 
+                <button onClick={() => onChange("")} type="button"
+                className="absolute right-0 top-0 rounded-full bg-rose-500 p-1 text-white shadow-sm">
+                    <X className="h-4 w-4"/>
+                </button>
+            </div>
+        )
+    }
+
     return (
         <UploadDropzone endpoint={endpoint}
             onClientUploadComplete={(res) => {
