76ae284b4b32b081941bb4e8371d27d4d6e5bf13
BdNWoG <billy.gao@hotmail.com>
2024-05-21 12:03:46 -0700

    nav button

--- harmonic/middleware.ts
+++ harmonic/middleware.ts
@@ -1,9 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-    publicRoutes: ["/api/uploadthing"]
+const isPublicRoute = createRouteMatcher(["/api/uploadthing"]);
+
+export default clerkMiddleware((auth, request) => {
+    if(!isPublicRoute(request)) {
+        auth().protect();
+    }
 });
 
 export const config = {
-    matcher: ["/((?!.*\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
