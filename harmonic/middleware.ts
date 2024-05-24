c2c4723f0b584e76333f78266bdbe096c1039e13
BdNWoG <billy.gao@hotmail.com>
2024-05-23 18:39:18 -0700

    bug fixed

--- harmonic/middleware.ts
+++ harmonic/middleware.ts
@@ -1,13 +1,19 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/uploadthing"]);
-
-export default clerkMiddleware((auth, request) => {
-    if(!isPublicRoute(request)) {
-        auth().protect();
-    }
+export default authMiddleware({
+    publicRoutes:["/api/uploadthing"]
 });
 
 export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)', '/sign-in'],
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isPublicRoute = createRouteMatcher(["/api/uploadthing"]);
+
+// export default clerkMiddleware((auth, request) => {
+//     if(!isPublicRoute(request)) {
+//         auth().protect();
+//     }
+// });
