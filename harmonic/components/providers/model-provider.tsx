ece04ef187dc7f9b5044d39544a23ff70a1f11d4
BdNWoG <billy.gao@hotmail.com>
2024-05-23 20:38:14 -0700

    edit

--- harmonic/components/providers/model-provider.tsx
+++ harmonic/components/providers/model-provider.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { CreateServerModel } from "../models/create-server-model";
 import { InviteModel } from "../models/invite-model";
+import { EditServerModel } from "../models/edit-server-model";
 
 export const ModelProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -19,6 +20,7 @@ export const ModelProvider = () => {
         <>
             <CreateServerModel />
             <InviteModel />
+            <EditServerModel />
         </>
     )
 };
