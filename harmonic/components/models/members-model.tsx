93cc2ec578734d46f5745bf65473b26687e7ab5e
BdNWoG <billy.gao@hotmail.com>
2024-05-24 17:20:31 -0700

    kick

--- harmonic/components/models/members-model.tsx
+++ harmonic/components/models/members-model.tsx
@@ -27,6 +27,27 @@ export const MembersModel = () => {
     const isModelOpen = isOpen && type === "members";
     const { server } = data as { server: ServerWithMembersWithProfiles};
 
+    const onKick = async (memberId: string) => {
+        try {
+            setLoadingId(memberId);
+            const url = qs.stringifyUrl({
+                url: `/api/members/${memberId}`,
+                query: {
+                    serverId: server?.id,
+                }
+            });
+
+            const response = await axios.delete(url);
+
+            router.refresh();
+            onOpen("members", { server: response.data });
+        } catch (error) {
+            console.log(error); 
+        } finally {
+            setLoadingId("");
+        }
+    }
+
     const onRoleChange = async (memberId: string, role: MemberRole) => {
         try {
             setLoadingId(memberId);
@@ -104,7 +125,8 @@ export const MembersModel = () => {
                                                 </DropdownMenuPortal>
                                             </DropdownMenuSub>
                                             <DropdownMenuSeparator/>
-                                            <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                                            <DropdownMenuItem onClick={() => onKick(member.id)}
+                                            className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                                                 <Gavel className="h-4 w-4 mr-2"/>
                                                 Kick
                                             </DropdownMenuItem>
