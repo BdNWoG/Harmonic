dd1669e36126a75f0f7e2a979c20ba5d717e0250
BdNWoG <billy.gao@hotmail.com>
2024-05-26 22:43:14 -0700

    channel

--- harmonic/components/chat/chat-messages.tsx
+++ harmonic/components/chat/chat-messages.tsx
@@ -2,6 +2,8 @@
 
 import { Member } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
 
 interface ChatMessagesProps {
     name: string,
@@ -18,6 +20,32 @@ interface ChatMessagesProps {
 export const ChatMessages = ({
     name, member, chatId, apiUrl, socketUrl, socketQuery, paramKey, paramValue, type
 }: ChatMessagesProps) => {
+    const queryKey = `chat:${chatId}`;
+    const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status} = useChatQuery({ queryKey, apiUrl, paramKey, paramValue });
+
+    //@ts-ignore
+    if (status === "loading") {
+        return (
+            <div className="flex-1 flex flex-col justify-center items-center">
+                <Loader2 className="w-7 h-7 text-zinc-500 animate-spin my-4"/>
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Loading Messages...
+                </p>
+            </div>
+        )
+    } 
+
+    if (status === "error") {
+        return (
+            <div className="flex-1 flex flex-col justify-center items-center">
+                <ServerCrash className="w-7 h-7 text-zinc-500 my-4"/>
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Something Went Wrong!
+                </p>
+            </div>
+        )
+    } 
+
     return (
         <div className="flex-1 flex flex-col py-4 overflow-y-auto">
             <div className="flex-1"/>
