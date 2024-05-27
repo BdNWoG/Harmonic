ad09fae279ae38adaecc89aabab0c7c54a9272c2
BdNWoG <billy.gao@hotmail.com>
2024-05-27 15:02:51 -0700

    chat

---
+++ harmonic/components/chat/chat-item.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { Member, Profile } from "@prisma/client"
+import { UserAvatar } from "../user-avatar";
+
+interface ChatItemProps {
+    id: string,
+    content: string, 
+    member: Member & {
+        profile: Profile
+    };
+    timestamp: string, 
+    fileUrl: string | null,
+    deleted: boolean, 
+    currentMember: Member, 
+    isUpdated: boolean, 
+    socketUrl: string, 
+    socketQuery: Record<string, string>, 
+}
+
+export const ChatItem = ({
+    id, content, member, timestamp, fileUrl, deleted, currentMember, isUpdated, socketUrl, socketQuery
+}: ChatItemProps) => {
+    return (
+        <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
+            <div className="group flex gap-x-2 items-start w-full">
+                <div className="cursor-pointer hover:drop-shadow-md transition">
+                    <UserAvatar src={member.profile.imageUrl}/>
+                </div>
+                <div className="flex flex-col w-full">
+                    <div className="flex items-center gap-x-2">
+                        <div className="flex items-center">
+                            <p>
+                                {member.profile.name}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- harmonic/components/chat/chat-messages.tsx
+++ harmonic/components/chat/chat-messages.tsx
@@ -5,6 +5,10 @@ import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import { ChatItem } from "./chat-item";
+import { format } from "date-fns"
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm:ss"
 
 type MessageWithMemberWithProfile = Message & {
     member: Member & {
@@ -61,9 +65,11 @@ export const ChatMessages = ({
                 {data?.pages?.map((group, i) => (
                     <Fragment key={i}>
                         {group.items.map((message: MessageWithMemberWithProfile) =>(
-                            <div key={message.id}>
-                                {message.content}
-                            </div>
+                            <ChatItem currentMember={member} key={message.id} id={message.id}
+                            content={message.content} fileUrl={message.fileUrl} deleted={message.deleted} 
+                            timestamp={format(new Date(message.createdAt), DATE_FORMAT)} 
+                            isUpdated={message.updatedAt !== message.createdAt} socketUrl={socketUrl} 
+                            socketQuery={socketQuery} member={message.member}/>
                         ))}
                     </Fragment>
                 ))}
